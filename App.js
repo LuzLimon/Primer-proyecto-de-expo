185472339975b2abed2b9ba1634867f0fcccf6b4
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-09-13 21:04:40 -0500

    Feat: Creation of the first expo project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Luz Elena Limón de la Cruz</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,8 +12,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
+    backgroundColor: '#F28FE3',
     alignItems: 'center',
     justifyContent: 'center',
   },
